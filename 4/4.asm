f377f8301fb003d852d09a71cb943daa40c01e80
Vineeth Raghavan <VineethRaghavan@users.noreply.github.com>
2022-09-28 23:03:00 +0530

    initial commit

---
+++ 4/4.asm
@@ -0,0 +1,109 @@
+COUT MACRO VAR
+	MOV AH, 2
+	MOV DL, VAR
+	INT 21H
+ENDM
+.MODEL SMALL
+
+.DATA
+	N DB 8
+	R DB 4
+	RES DB ?
+.CODE
+	MOV AX, @DATA
+	MOV DS, AX
+
+	MOV RES, 0
+	MOV AH, N
+	MOV AL, R
+
+	CALL NCR
+
+	MOV AL, N
+	MOV AH, 0
+	CALL DISPLAY
+
+	COUT 'C'
+
+	MOV AL, R
+	MOV AH, 0
+	CALL DISPLAY
+
+	COUT ' '
+	COUT '='
+	COUT ' '
+
+	MOV AL, RES
+	MOV AH, 0
+	CALL DISPLAY
+
+	JMP EOF
+
+	NCR PROC NEAR
+		
+		
+		CMP AL, 0
+			JE ONE
+
+		
+		CMP AL, AH
+			JE ONE
+
+		
+		CMP AL, 1
+			JE EN
+
+		MOV BH, AH
+		DEC BH
+		
+		CMP AL, BH 
+			JE EN
+		
+		DEC AH
+		PUSH AX
+		CALL NCR
+		POP AX
+
+		PUSH AX
+		DEC AL
+		CALL NCR
+		POP AX
+		RET
+
+		ONE:
+			ADD RES, 1
+			RET 
+			
+		EN:
+			ADD RES, AH
+			RET 
+	NCR ENDP
+
+	DISPLAY PROC NEAR
+		PUSH CX
+
+		MOV BX, 10    
+		MOV DX, 0000H   
+		MOV CX, 0000H   
+    		
+		L1:  MOV DX, 0000H   
+		DIV BX      
+		PUSH DX    
+		INC CX    
+		CMP AX, 0     
+		JNE L1    
+		L2:
+		POP DX      
+		ADD DX, 30H   
+		MOV AH, 02H     
+		INT 21H     
+		LOOP L2 
+
+		POP CX
+		RET 
+	DISPLAY ENDP
+
+	EOF:
+		MOV AH, 4CH
+		INT 21H
+END
